9b3bcf6b3462a66ccfa2b74ab336c007a92265e8
Shubhra7 <shubhrajit.g2002@gmail.com>
2025-06-23 12:33:56 +0530

    Add routing with react-router-dom and create Display components

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Sidebar from './Sidebar'
 import Player from './Player'
+import Display from './Display'
 
 const Home = () => {
   return (
     <div className="h-screen bg-black">
         <div className='h-[90%] flex'>
                 <Sidebar/>
+                <Display/>
         </div>
         <Player/>
     </div>
