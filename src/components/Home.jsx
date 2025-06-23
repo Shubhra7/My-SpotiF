10834f7570e6b3eb032fc2fff61b7e92bdf47524
Shubhra7 <shubhrajit.g2002@gmail.com>
2025-06-23 11:35:25 +0530

    Add Home and Sidebar components to the application

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Sidebar from './Sidebar'
+
+const Home = () => {
+  return (
+    <div className="h-screen bg-black">
+      <div className='h-[90%] flex'>
+            <Sidebar/>
+      </div>
+    </div>
+  )
+}
+
+export default Home
