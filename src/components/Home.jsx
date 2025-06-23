7ef9d98710e85c7281a5b59e29e31f26f0d024f8
Shubhra7 <shubhrajit.g2002@gmail.com>
2025-06-23 12:00:08 +0530

    Add Player component and integrate it into Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Sidebar from './Sidebar'
+import Player from './Player'
 
 const Home = () => {
   return (
     <div className="h-screen bg-black">
-      <div className='h-[90%] flex'>
-            <Sidebar/>
-      </div>
+        <div className='h-[90%] flex'>
+                <Sidebar/>
+        </div>
+        <Player/>
     </div>
   )
 }
